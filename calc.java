5b9cbef41020fda15a5af4104201bf0f32186081
Peter McLaughlin <Peter.McLaughlin.ie>
2018-11-25 15:31:57 +0000

    Intital commit of project

---
+++ calc.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;  /*used for input*/
+
+public class calc
+{
+
+	public static void main(String[] args)
+	{
+	int num1, num2;
+
+	Scanner input = new Scanner (System.in);
+	System.out.println("enter num 1: ");
+	num1 = input.nextInt();
+	System.out.println("enter num 2: ");
+ 	num2 = input.nextInt();
+	
+	System.out.println("answer " + (num1 + num2));
+	}
+
+}
+
